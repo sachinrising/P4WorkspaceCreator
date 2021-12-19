b61947adb212e781a27df02c231eaa146acd6809
Sachin Srivastava <sachinrising@gmail.com>
2021-12-19 07:33:25 +0530

    Fixed issue with the commandline when it is not able to find the module

--- P4WorkspaceCreator/__main__.py
+++ P4WorkspaceCreator/__main__.py
@@ -1,5 +1,5 @@
 import sys
 
 if __name__ == "__main__":
-    from WorkspaceCreator import main as _main
+    from P4WorkspaceCreator.WorkspaceCreator import main as _main
     sys.exit(_main(sys.argv[1:]))
